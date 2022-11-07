2e41d0b40658cba8ccd2ce238b2772760ceb1e22
Rushikesh041 <jadhavrushi041@gmail.com>
2022-11-07 19:01:35 +0530

    UC2TC2Float

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -4,19 +4,13 @@
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
             Console.WriteLine("First number is largest :  " + compare.Largest(60, 50, 20));
-=======
+
             Console.WriteLine("Third number is largest :  " + compare.Largest(40, 30, 60));
->>>>>>> UC1TC3Integer
-=======
+
             Console.WriteLine("Second number is largest :  " + compare.Largest(40, 60, 20));
->>>>>>> UC1TC2Integer
-=======
-            Console.WriteLine("largest number is  :  " + compare.Largest(60.2f, 50.5f, 30.3f));
->>>>>>> UC2TC1Float
+
+            Console.WriteLine("largest number is  :  " + compare.Largest(60.2f, 70.5f, 30.3f));
         }
     }
 }
