58d4f7de2f3d6b05bb02fdc688af8b3f1f8d3af9
Rushikesh041 <jadhavrushi041@gmail.com>
2022-11-07 18:52:56 +0530

    UC2TC1Float

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("largest number is  :  " + compare.Largest(60.2f, 50.5f, 20.3f));
+            Console.WriteLine("largest number is  :  " + compare.Largest(60.2f, 50.5f, 30.3f));
         }
     }
 }
