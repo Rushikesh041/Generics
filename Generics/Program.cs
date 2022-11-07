8549bed412b79ef8a01195f029851a72e5bfdf3f
Rushikesh041 <jadhavrushi041@gmail.com>
2022-11-07 18:10:38 +0530

    UC1TC3Integer

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Third number is largest :  " + compare.Largest(40, 30, 50));
+            Console.WriteLine("Third number is largest :  " + compare.Largest(40, 30, 60));
         }
     }
 }
