c26804a5c3788d028da7222c7ef978e2450657a4
Rushikesh041 <jadhavrushi041@gmail.com>
2022-11-07 17:36:06 +0530

    UC1TC1Integer

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("  First number is largest :  " + compare.Largest(40, 30, 20));
+            Console.WriteLine("First number is largest :  " + compare.Largest(40, 30, 20));
         }
     }
 }
