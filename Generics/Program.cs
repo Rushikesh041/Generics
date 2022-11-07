66330866076fc397d73169f2fab26a9ac2365ce4
Rushikesh041 <jadhavrushi041@gmail.com>
2022-11-07 19:52:48 +0530

    UC3TC3String

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Largest string is  :  " + compare.Largest("Latur","Aurangabad","Pune")); 
+            Console.WriteLine("Largest string is  :  " + compare.Largest("Goa","Punjab","Maharashtra")); 
         }
     }
 }
