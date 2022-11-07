6f29547d9c9d3d397850c0ec1cfc393cdaa53021
Rushikesh041 <jadhavrushi041@gmail.com>
2022-11-07 19:38:37 +0530

    UC3TC2String

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -4,17 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("First number is largest :  " + compare.Largest(60, 50, 20));
-
-            Console.WriteLine("Third number is largest :  " + compare.Largest(40, 30, 60));
-
-            Console.WriteLine("Second number is largest :  " + compare.Largest(40, 60, 20));
-
-<<<<<<< HEAD
-            Console.WriteLine("largest number is  :  " + compare.Largest(60.2f, 70.5f, 30.3f));
-=======
-            Console.WriteLine("largest number is  :  " + compare.Largest(60.2f, 80.5f, 90.3f));
->>>>>>> UC2TC3Float
+            Console.WriteLine("Largest string is  :  " + compare.Largest("Latur","Aurangabad","Pune")); 
         }
     }
 }
